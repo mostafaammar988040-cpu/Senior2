4296d86c1ea423151989c42c3f7e202ef45eae60
Abbas310 <abbashousseiny.2005@gmail.com>
2026-02-20 23:27:59 +0200

    Added Transportation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,34 +8,25 @@ import AdminReviews from "./pages/AdminReviews";
 import AdminUsers from "./pages/AdminUsers";
 import Preferences from "./pages/Preferences";
 import Introduction from "./pages/Introduction";
-import AIAssistant from "./pages/ai-assistant/AIAssistant";
-import Events from "./pages/Events";
-import SmartItineraryintro from "./pages/SmartItineraryintro";
-
-import SmartItineraryForm from "./pages/SmartItineraryForm";
-
-
+import TaxiServices from "./pages/TaxiServices";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/SmartItinerary" element={<SmartItineraryForm />} />
-      <Route path="/SmartItineraryintro" element={<SmartItineraryintro />} />
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/events" element={<Events />} />
-<Route path="/admin" element={<AdminLayout />}>
 
-  <Route index element={<AdminDashboard />} />
-  <Route path="reviews" element={<AdminReviews />} />
-  <Route path="users" element={<AdminUsers />} />
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="reviews" element={<AdminReviews />} />
+        <Route path="users" element={<AdminUsers />} />
+      </Route>
 
-</Route>
-<Route path="/preferences" element={<Preferences />} />
-<Route path="/introduction" element={<Introduction />} />
-<Route path="/ai-assistant" element={<AIAssistant/>}/>
+      <Route path="/preferences" element={<Preferences />} />
+      <Route path="/introduction" element={<Introduction />} />
+      <Route path="/taxis" element={<TaxiServices />} />
     </Routes>
   );
 }
