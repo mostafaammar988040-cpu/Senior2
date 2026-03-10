80ee9ba4c57f5b9308ae166693ee5156363d51b0
Abbas310 <abbashousseiny.2005@gmail.com>
2026-03-10 20:51:40 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7090/api",
+  baseURL: "http://localhost:5262/api",
   headers: {
     "Content-Type": "application/json",
   },
